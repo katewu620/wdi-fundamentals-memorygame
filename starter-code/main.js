662760e18c3c1a19a11cb795f301d5abc18d2b08
Yurou Wu <yurou.wu@gmail.com>
2016-10-12 10:20:32 -0700

    update game logic

--- starter-code/main.js
+++ starter-code/main.js
@@ -1,1 +1,12 @@
 console.log("JS file is connected to HTML! Woo!")
+
+var cardOne = "king";
+var cardTwo = "king";
+var cardThree = "queen";
+var cardFour = "queen";
+
+if (cardOne === cardTwo){
+	alert("You found a match!");
+} else {
+	alert('Sorry, try again.');
+}
